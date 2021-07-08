4d556b0bf99b1d8a48cb15f621501792a046829a
桂华新 <525901934@qq.com>
2021-07-08 15:11:55 +0800

    eurekaserver搭建完成

---
+++ springcloud-eureka-8082/src/main/java/com/guihx/springcloud/EurekaServer_8082.java
@@ -0,0 +1,13 @@
+package com.guihx.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer_8082 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer_8082.class, args);
+    }
+}
