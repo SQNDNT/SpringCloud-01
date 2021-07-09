03021815eecabe37b5df6ab2ad8eb21bf59a4bc2
桂华新 <525901934@qq.com>
2021-07-09 10:16:25 +0800

    搞3个eureka注册中心，准备搞集群

--- springcloud-eureka-8084/src/main/java/com/guihx/springcloud/EurekaServer_8084.java
+++ springcloud-eureka-8084/src/main/java/com/guihx/springcloud/EurekaServer_8084.java
@@ -2,12 +2,12 @@ package com.guihx.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class DeptProvider_8080 {
+@EnableEurekaServer
+public class EurekaServer_8084 {
     public static void main(String[] args) {
-        SpringApplication.run(DeptProvider_8080.class,args);
+        SpringApplication.run(EurekaServer_8084.class, args);
     }
 }
