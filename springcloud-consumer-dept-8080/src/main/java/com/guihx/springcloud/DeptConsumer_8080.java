0967e48fffb7db41aa5da04d034578e9aeca17b6
桂华新 <525901934@qq.com>
2021-07-12 16:16:11 +0800

    端口规范点

---
+++ springcloud-consumer-dept-8080/src/main/java/com/guihx/springcloud/DeptConsumer_8080.java
@@ -0,0 +1,13 @@
+package com.guihx.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class DeptConsumer_8080 {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer_8080.class,args);
+    }
+}
