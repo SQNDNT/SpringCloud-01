0304b43dc79750180c9e2b7d25f6572e9ae75a8a
桂华新 <525901934@qq.com>
2021-07-13 14:10:44 +0800

    增加第二个提供者，ribbon默认轮询

---
+++ springcloud-provider-dept-8002/src/main/java/springcloud/controller/DeptController.java
@@ -0,0 +1,34 @@
+package springcloud.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.guihx.springcloud.pojo.Dept;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import springcloud.service.DeptService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RestController
+@RequestMapping("/cloud/dept")
+public class DeptController {
+    @Autowired
+    DeptService deptService;
+
+    @RequestMapping("/getAll")
+    public List<Dept> getAll() {
+        return deptService.list();
+    }
+
+    @GetMapping("/get/{id}")
+    public Dept getById(@PathVariable("id") Long id) {
+        return deptService.getById(id);
+    }
+
+    @GetMapping("/get2/{id}")
+    public Dept getById(@PathVariable("id") Long id, @RequestParam("name") String name, HttpServletRequest request) {
+        System.out.println(request.getRequestURI());
+        return deptService.getOne(new QueryWrapper<Dept>().lambda().eq(Dept::getId, id).like(Dept::getName, name));
+    }
+
+}
