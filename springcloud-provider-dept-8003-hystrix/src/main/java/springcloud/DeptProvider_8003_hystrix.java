655828a542ed58db374b1b6973347065075bc6a5
桂华新 <525901934@qq.com>
2021-07-16 10:25:07 +0800

    hystrix-dashboard

--- springcloud-provider-dept-8003-hystrix/src/main/java/springcloud/DeptProvider_8003_hystrix.java
+++ springcloud-provider-dept-8003-hystrix/src/main/java/springcloud/DeptProvider_8003_hystrix.java
@@ -1,9 +1,12 @@
 package springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,5 +14,12 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 public class DeptProvider_8003_hystrix {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_8003_hystrix.class,args);
+    }
+    //这个bean死东西照抄就完了
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        return registrationBean;
     }
 }
